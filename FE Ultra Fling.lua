01f711643a7997c3583f29482bb2b9289258f7f3
Misakiii <59036402+Miisaakii@users.noreply.github.com>
2023-04-03 12:38:14 +0200

    Update FE Ultra Fling.lua

--- FE Ultra Fling.lua
+++ FE Ultra Fling.lua
@@ -1,9 +1,12 @@
-local toggle = false --Toggle
-local speed = 0.4 --Increases/Decreases Speed
+--Script made by Misaki#9446
+
+local toggle = false --Toggle random teleport and fling
+local speed = 0.1 --Increases/Decreases Speed
 local positionY = 0
 
 local LocalPlayer =  game.Players.LocalPlayer
 local RunService = game:GetService("RunService")
+local TeleportService = game:GetService("TeleportService")
 
 function Notification(title, text)
     game:GetService("StarterGui"):SetCore("SendNotification",{ Title = title, Text = text, Icon = "rbxthumb://type=Asset&id=12952454903&w=150&h=150", Duration = 10})
@@ -22,21 +25,19 @@ key.InputBegan:Connect(function(input)
             Notification("FE Ultra Fling", "Ultra Fling Enabled")
         end
 	end
+
+    if input.KeyCode == Enum.KeyCode.O then
+        TeleportService:Teleport(game.PlaceId, LocalPlayer)
+	end
 end)
 
-function fling_script(player)
-    local BodyAngularVelocity = Instance.new("BodyAngularVelocity")
-    BodyAngularVelocity.AngularVelocity = Vector3.new(10^6, 10^6, 10^6)
-    BodyAngularVelocity.MaxTorque = Vector3.new(10^6, 10^6, 10^6)
-    BodyAngularVelocity.P = 10^6
-
-    while true do
-        RunService.RenderStepped:Wait()
-        LocalPlayer.Character.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame * LocalPlayer.Character.HumanoidRootPart.CFrame.Rotation
-        LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new()
-    end
-
-    BodyAngularVelocity.Parent = nil
+function fling_script(target)
+    power = 500
+    local bambam = Instance.new("BodyThrust")
+    bambam.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
+    bambam.Force = Vector3.new(power,0,power)
+    LocalPlayer.Character.HumanoidRootPart.CFrame = target.Character.HumanoidRootPart.CFrame
+    bambam.Location = target.Character.HumanoidRootPart.Position
 end
 
 function random_telport()
@@ -47,7 +48,7 @@ function random_telport()
                     spawn(function()
                         local Target = game.Players:GetPlayers()
                         [math.random(1,#game.Players:GetPlayers())]
-                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(Target.Character.Head.Position.X, Target.Character.Head.Position.Y - positionY, Target.Character.Head.Position.Z))
+                        fling_script(Target)
                         task.wait(speed)
                     end)
                 end
